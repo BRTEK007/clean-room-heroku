567a64a26a0e778c0ceab4eb54086a7630dbbfd9
BRTEK007 <bartoszkonderla@gmail.com>
2021-04-01 12:06:55 +0200

    changed port

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
+var PORT = process.env.PORT || 3000;
 var http = require('http');
 
 http.createServer(function (req, res) {
   res.write('Hello World!');
   res.end(); 
-}).listen(8080);
+}).listen(PORT);
